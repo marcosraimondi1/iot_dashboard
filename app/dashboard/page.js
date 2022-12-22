590dd6dee442a87ca5151e7a5ff6f1204f018df8
Marcos Raimondi <marcosraimondi1@hotmail.com>
2022-12-22 17:33:15 -0300

    dashboard with demo widgets

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,4 +1,29 @@
 "use client";
+import IotButton from "../widgets/IotButton";
+import IotIndicator from "../widgets/IotIndicator";
+import IotSwitch from "../widgets/IotSwitch";
+import Rtnumberchart from "../widgets/Rtnumberchart";
+import Grid from "@mui/material/Grid";
+
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const config = {
+    selectedDevice: { name: "Horno" },
+    variableFullName: "Temp",
+  };
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={3}>
+        <IotButton config={config} />
+      </Grid>
+      <Grid item xs={3}>
+        <IotIndicator config={config} />
+      </Grid>
+      <Grid item xs={3}>
+        <IotSwitch config={config} />
+      </Grid>
+      <Grid item xs={12}>
+        <Rtnumberchart config={config} />
+      </Grid>
+    </Grid>
+  );
 }
