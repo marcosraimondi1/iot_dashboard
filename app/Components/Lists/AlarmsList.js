b1635d8dd1639219a7fb8f1087e94df7eb78a407
Marcos Raimondi <marcosraimondi1@hotmail.com>
2022-12-26 15:20:49 -0300

    alarms list

---
+++ app/Components/Lists/AlarmsList.js
@@ -0,0 +1,90 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import PropTypes from "prop-types";
+import IconButton from "@mui/material/IconButton";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import Switch from "@mui/material/Switch";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+
+Lista.propTypes = {
+  alarms: PropTypes.arrayOf(PropTypes.object),
+  updateStatusRule: PropTypes.func,
+  del: PropTypes.func,
+};
+
+export default function Lista({ alarms, updateStatusRule, del }) {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">Alarm</TableCell>
+            <TableCell align="center">Variable</TableCell>
+            <TableCell align="center">Condition&nbsp;</TableCell>
+            <TableCell align="center">Value</TableCell>
+            <TableCell align="center">Trigger Time (min)&nbsp;</TableCell>
+            <TableCell align="center">Matches&nbsp;</TableCell>
+            <TableCell align="center">Status&nbsp;</TableCell>
+            <TableCell align="right">Eliminar&nbsp;</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {alarms.map((alarm, index) => (
+            <TableRow
+              key={index}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" align="left" scope="row">
+                {index}
+              </TableCell>
+
+              <TableCell component="th" align="center" scope="row">
+                {alarm.variableFullName}
+              </TableCell>
+
+              <TableCell align="center">{alarm.condition}</TableCell>
+              <TableCell align="center">{alarm.value}</TableCell>
+              <TableCell align="center">{alarm.triggerTime}</TableCell>
+              <TableCell align="center">{alarm.counter}</TableCell>
+
+              <TableCell align="center">
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-evenly",
+                  }}
+                >
+                  <NotificationsActiveIcon
+                    color={alarm.status ? "success" : "warning"}
+                  />
+                  <Switch
+                    color="success"
+                    checked={alarm.status}
+                    onChange={() => updateStatusRule(alarm)}
+                  />
+                </div>
+              </TableCell>
+
+              <TableCell align="right">
+                <IconButton
+                  onClick={() => {
+                    del(alarm);
+                  }}
+                >
+                  <DeleteForeverIcon sx={{ color: "red" }} />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- app/dashboard/alarms/page.js
+++ app/dashboard/alarms/page.js
@@ -5,6 +5,8 @@ import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
+import AlarmsList from "../../Components/Lists/AlarmsList";
+import randomString from "../../utils/randomString";
 
 const demoDevice = {
   name: "Home",
@@ -30,6 +32,23 @@ const demoDevice = {
           },
         },
       },
+      {
+        type: "Rtnumberchart",
+        config: {
+          variableFullName: "Humedad",
+          variable: "afdsuye",
+          icon: "shower",
+          color: "primary",
+          colSize: 12,
+          unit: "°C",
+          chartTimeAgo: 10,
+          variableSendFreq: 10,
+          decimalPlaces: 2,
+          selectedDevice: {
+            name: "Home",
+          },
+        },
+      },
     ],
   },
   password: "HLr3",
@@ -39,22 +58,60 @@ const demoDevice = {
   },
 };
 
-const newRule = {
-  dId: null,
-  status: true,
-  variableFullName: null,
-  deviceName: null,
-  variable: null,
-  value: null,
-  condition: null,
-  triggerTime: null,
-};
-
-const demoRules = [];
+const demoRules = [
+  {
+    _id: "ayf3cF08t2",
+    dId: "1",
+    status: false,
+    variableFullName: "Humedad",
+    deviceName: "Home",
+    variable: "afdsuye",
+    value: "26",
+    condition: "<=",
+    triggerTime: "10",
+    counter: 0,
+  },
+  {
+    _id: "Rd4xFMv6MT",
+    dId: "1",
+    status: true,
+    variableFullName: "Humedad",
+    deviceName: "Home",
+    variable: "afdsuye",
+    value: "55",
+    condition: ">=",
+    triggerTime: "5",
+    counter: 0,
+  },
+  {
+    _id: "uu1O8HE26G",
+    dId: "1",
+    status: false,
+    variableFullName: "Temperature",
+    deviceName: "Home",
+    variable: "streqert",
+    value: "24",
+    condition: ">",
+    triggerTime: "5",
+    counter: 0,
+  },
+  {
+    _id: "4z8TsQCy4e",
+    dId: "1",
+    status: true,
+    variableFullName: "Temperature",
+    deviceName: "Home",
+    variable: "streqert",
+    value: "15",
+    condition: "<",
+    triggerTime: "4",
+    counter: 0,
+  },
+];
 
 export default function Alarms() {
   const [device, setDevice] = useState(demoDevice);
-  const [rules, setRules] = useState(demoRules);
+  const [alarms, setAlarms] = useState(demoRules);
   const [selectedWidgetIndex, setSelectedWidgetIndex] = useState("");
   const [condition, setCondition] = useState("");
 
@@ -65,16 +122,26 @@ export default function Alarms() {
     // update rule status on db
     let newRule = JSON.parse(JSON.stringify(rule));
     newRule.status = !newRule.status;
+    let newRules = alarms.map((alarm) => {
+      if (alarm._id !== rule._id) return alarm;
+      return newRule;
+    });
+    setAlarms(newRules);
+    console.log(alarms);
   };
 
   const deleteRule = (rule) => {
     // delete rule from db
+    let newRules = alarms.filter((alarm) => alarm._id !== rule._id);
+    setAlarms(newRules);
   };
 
   const addRule = () => {
+    // add rule to db
     const { variable, variableFullName } =
-      device.template.widgets[selectedWidgetIndex];
+      device.template.widgets[selectedWidgetIndex].config;
     let newRule = {
+      _id: randomString(10),
       dId: device.dId,
       status: true,
       variableFullName,
@@ -83,9 +150,10 @@ export default function Alarms() {
       value: valueRef.current,
       condition,
       triggerTime: triggerTimeRef.current,
+      counter: 0,
     };
-    let newRules = rules.concat(newRule);
-    setRules(newRules);
+    let newRules = alarms.concat(newRule);
+    setAlarms(newRules);
   };
 
   return (
@@ -94,7 +162,7 @@ export default function Alarms() {
         <Grid container spacing={1}>
           <Grid item xs={12}>
             <div style={{ marginLeft: "20px" }}>
-              <h2>Add New Rule</h2>
+              <h2>Add New Alarm</h2>
             </div>
           </Grid>
 
@@ -180,6 +248,11 @@ export default function Alarms() {
         </Grid>
       </Card>
       <br />
+      <AlarmsList
+        alarms={alarms}
+        updateStatusRule={updateStatusRule}
+        del={deleteRule}
+      />
     </>
   );
 }
