17003d27fdfb85ab928e354dc90c16d6acdc7506
Marcos Raimondi <marcosraimondi1@hotmail.com>
2022-12-26 13:50:56 -0300

    devices list

---
+++ app/Components/Lists/DevicesList.js
@@ -0,0 +1,115 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import PropTypes from "prop-types";
+import IconButton from "@mui/material/IconButton";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import Switch from "@mui/material/Switch";
+import StorageIcon from "@mui/icons-material/Storage";
+
+Lista.propTypes = {
+  devices: PropTypes.arrayOf(PropTypes.object),
+  refreshPassword: PropTypes.func,
+  updateSaverRuleStatus: PropTypes.func,
+  del: PropTypes.func,
+};
+
+export default function Lista({
+  devices,
+  refreshPassword,
+  updateSaverRuleStatus,
+  del,
+}) {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">Device</TableCell>
+            <TableCell align="center">Name</TableCell>
+            <TableCell align="center">Id&nbsp;</TableCell>
+            <TableCell align="center">Password</TableCell>
+            <TableCell align="center">Template Name&nbsp;</TableCell>
+            <TableCell align="center">Database&nbsp;</TableCell>
+            <TableCell align="right">Eliminar&nbsp;</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {devices.map((device, index) => (
+            <TableRow
+              key={index}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" align="left" scope="row">
+                {index}
+              </TableCell>
+
+              <TableCell component="th" align="center" scope="row">
+                {device.name}
+              </TableCell>
+
+              <TableCell align="center">{device.dId}</TableCell>
+
+              <TableCell align="center">
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-evenly",
+                  }}
+                >
+                  <p>{device.password}</p>
+                  <IconButton
+                    onClick={() => {
+                      refreshPassword(device.dId);
+                    }}
+                  >
+                    <RefreshIcon color="info" />
+                  </IconButton>
+                </div>
+              </TableCell>
+
+              <TableCell align="center">{device.template.name}</TableCell>
+
+              <TableCell align="center">
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-evenly",
+                  }}
+                >
+                  <StorageIcon
+                    color={device.saverRule.status ? "success" : "warning"}
+                  />
+                  <Switch
+                    color="success"
+                    checked={device.saverRule.status}
+                    onChange={() => updateSaverRuleStatus(device.saverRule)}
+                  />
+                </div>
+              </TableCell>
+
+              <TableCell align="right">
+                <IconButton
+                  onClick={() => {
+                    del(device.dId);
+                  }}
+                >
+                  <DeleteForeverIcon sx={{ color: "red" }} />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- app/dashboard/devices/page.js
+++ app/dashboard/devices/page.js
@@ -5,6 +5,8 @@ import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
+import DevicesList from "../../Components/Lists/DevicesList";
+import randomString from "../../utils/randomString";
 
 const demoTemplates = [
   {
@@ -78,14 +80,302 @@ const demoTemplates = [
   },
 ];
 
+const demoDevices = [
+  {
+    name: "Home",
+    dId: "1",
+    template: {
+      name: "Template 1",
+      _id: 1,
+      widgets: [
+        {
+          type: "Rtnumberchart",
+          config: {
+            variableFullName: "Temperature",
+            icon: "shower",
+            color: "primary",
+            colSize: 12,
+            unit: "°C",
+            chartTimeAgo: 10,
+            variableSendFreq: 10,
+            decimalPlaces: 2,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+      ],
+    },
+    password: "HLr3",
+    saverRule: {
+      dId: "1",
+      status: false,
+    },
+  },
+  {
+    name: "Kitchen",
+    dId: "2",
+    template: {
+      name: "Template 2",
+      _id: 2,
+      widgets: [
+        {
+          type: "Rtnumberchart",
+          config: {
+            variableFullName: "Temperature",
+            icon: "shower",
+            color: "primary",
+            colSize: 12,
+            unit: "°C",
+            chartTimeAgo: 10,
+            variableSendFreq: 10,
+            decimalPlaces: 2,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+        {
+          type: "IotSwitch",
+          config: {
+            variableFullName: "Pump",
+            icon: "shop",
+            color: "error",
+            colSize: 6,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+        {
+          type: "IotIndicator",
+          config: {
+            variableFullName: "Pump",
+            icon: "shop",
+            color: "success",
+            colSize: 6,
+            variableSendFreq: 10,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+      ],
+    },
+    password: "g7Pg",
+    saverRule: {
+      dId: "2",
+      status: true,
+    },
+  },
+  {
+    name: "Fish Tank",
+    dId: "3",
+    template: {
+      name: "Template 1",
+      _id: 1,
+      widgets: [
+        {
+          type: "Rtnumberchart",
+          config: {
+            variableFullName: "Temperature",
+            icon: "shower",
+            color: "primary",
+            colSize: 12,
+            unit: "°C",
+            chartTimeAgo: 10,
+            variableSendFreq: 10,
+            decimalPlaces: 2,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+      ],
+    },
+    password: "E7PG",
+    saverRule: {
+      dId: "3",
+      status: true,
+    },
+  },
+  {
+    name: "Bedroom",
+    dId: "4",
+    template: {
+      name: "Template 2",
+      _id: 2,
+      widgets: [
+        {
+          type: "Rtnumberchart",
+          config: {
+            variableFullName: "Temperature",
+            icon: "shower",
+            color: "primary",
+            colSize: 12,
+            unit: "°C",
+            chartTimeAgo: 10,
+            variableSendFreq: 10,
+            decimalPlaces: 2,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+        {
+          type: "IotSwitch",
+          config: {
+            variableFullName: "Pump",
+            icon: "shop",
+            color: "error",
+            colSize: 6,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+        {
+          type: "IotIndicator",
+          config: {
+            variableFullName: "Pump",
+            icon: "shop",
+            color: "success",
+            colSize: 6,
+            variableSendFreq: 10,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+      ],
+    },
+    password: "zG4E",
+    saverRule: {
+      dId: "4",
+      status: false,
+    },
+  },
+  {
+    name: "Test",
+    dId: "5",
+    template: {
+      name: "Template 2",
+      _id: 2,
+      widgets: [
+        {
+          type: "Rtnumberchart",
+          config: {
+            variableFullName: "Temperature",
+            icon: "shower",
+            color: "primary",
+            colSize: 12,
+            unit: "°C",
+            chartTimeAgo: 10,
+            variableSendFreq: 10,
+            decimalPlaces: 2,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+        {
+          type: "IotSwitch",
+          config: {
+            variableFullName: "Pump",
+            icon: "shop",
+            color: "error",
+            colSize: 6,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+        {
+          type: "IotIndicator",
+          config: {
+            variableFullName: "Pump",
+            icon: "shop",
+            color: "success",
+            colSize: 6,
+            variableSendFreq: 10,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+      ],
+    },
+    password: "0iMQ",
+    saverRule: {
+      dId: "5",
+      status: true,
+    },
+  },
+  {
+    name: "Test2",
+    dId: "6",
+    template: {
+      name: "Template 2",
+      _id: 2,
+      widgets: [
+        {
+          type: "Rtnumberchart",
+          config: {
+            variableFullName: "Temperature",
+            icon: "shower",
+            color: "primary",
+            colSize: 12,
+            unit: "°C",
+            chartTimeAgo: 10,
+            variableSendFreq: 10,
+            decimalPlaces: 2,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+        {
+          type: "IotSwitch",
+          config: {
+            variableFullName: "Pump",
+            icon: "shop",
+            color: "error",
+            colSize: 6,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+        {
+          type: "IotIndicator",
+          config: {
+            variableFullName: "Pump",
+            icon: "shop",
+            color: "success",
+            colSize: 6,
+            variableSendFreq: 10,
+            selectedDevice: {
+              name: "Home",
+            },
+          },
+        },
+      ],
+    },
+    password: "qH3w",
+    saverRule: {
+      dId: "6",
+      status: false,
+    },
+  },
+];
+
 export default function Devices() {
   const [templates, setTemplates] = useState(demoTemplates);
-  const [devices, setDevices] = useState([]);
+  const [devices, setDevices] = useState(demoDevices);
   const [deviceTemplateIndex, setDeviceTemplateIndex] = useState("");
 
   const deviceNameRef = useRef("");
   const deviceIdRef = useRef("");
-  const deviceTemplateIndexRef = useRef("");
 
   const addDevice = () => {
     // add device to db
@@ -94,171 +384,116 @@ export default function Devices() {
     let newDevice = {
       name: deviceNameRef.current,
       dId: deviceIdRef.current,
-      templateName: template.name,
-      templateId: template._id,
+      template: template,
+      password: "default",
+      saverRule: {
+        dId: deviceIdRef.current,
+        status: true,
+      },
     };
 
     let newDevices = devices.concat(newDevice);
     setDevices(newDevices);
+    console.log(devices);
   };
 
-  const deleteDevice = (delIndex) => {
+  const deleteDevice = (dId) => {
     // delete device from db
-    let newDevices = devices.filter((_val, index) => delIndex !== index);
+    let newDevices = devices.filter((device) => device.dId !== dId);
+    setDevices(newDevices);
+  };
+
+  const refreshPassword = (dId) => {
+    // refresh password in db
+    const toSend = {
+      dId,
+    };
+    let newDevices = devices.map((device) => {
+      if (device.dId !== dId) return device;
+      device.password = randomString(4);
+      return device;
+    });
+    setDevices(newDevices);
+    console.table(devices);
+  };
+
+  const updateSaverRuleStatus = (rule) => {
+    // update rule in db
+    let newRule = JSON.parse(JSON.stringify(rule));
+    newRule.status = !newRule.status;
+    let newDevices = devices.map((device) => {
+      if (device.dId !== rule.dId) return device;
+      device.saverRule = newRule;
+      return device;
+    });
     setDevices(newDevices);
   };
 
   return (
-    <Card title="">
-      <Grid container spacing={1}>
-        <Grid item xs={12}>
-          <div style={{ marginLeft: "20px" }}>
-            <h2>Add New Device</h2>
-          </div>
+    <>
+      <Card title="">
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <div style={{ marginLeft: "20px" }}>
+              <h2>Add New Device</h2>
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              required
+              onChange={(e) => (deviceNameRef.current = e.target.value)}
+              label="Device Name"
+              sx={{ width: "100%", margin: "10px" }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              required
+              onChange={(e) => (deviceIdRef.current = e.target.value)}
+              label="Device Id"
+              sx={{ width: "80%", margin: "10px" }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              required
+              select
+              label="Template"
+              value={deviceTemplateIndex}
+              onChange={(e) => {
+                setDeviceTemplateIndex(e.target.value);
+              }}
+              sx={{ width: "100%", margin: "10px" }}
+            >
+              {templates.map((temp, index) => (
+                <MenuItem key={index} value={index}>
+                  {temp.name}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={6}>
+            <div
+              style={{
+                display: "flex",
+                margin: "10px",
+                alignItems: "center",
+              }}
+            >
+              <Button variant="contained" onClick={addDevice}>
+                Add
+              </Button>
+            </div>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <TextField
-            required
-            onChange={(e) => (deviceNameRef.current = e.target.value)}
-            label="Device Name"
-            sx={{ width: "100%", margin: "10px" }}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            required
-            onChange={(e) => (deviceIdRef.current = e.target.value)}
-            label="Device Id"
-            sx={{ width: "100%", margin: "10px" }}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            required
-            select
-            label="Template"
-            value={deviceTemplateIndex}
-            onChange={(e) => {
-              setDeviceTemplateIndex(e.target.value);
-            }}
-            sx={{ width: "100%", margin: "10px" }}
-          >
-            {templates.map((temp, index) => (
-              <MenuItem key={index} value={index}>
-                {temp.name}
-              </MenuItem>
-            ))}
-          </TextField>
-        </Grid>
-        <Grid item xs={6}>
-          <div
-            style={{
-              display: "flex",
-              margin: "10px",
-              alignItems: "center",
-            }}
-          >
-            <Button variant="contained" onClick={addDevice}>
-              Add
-            </Button>
-          </div>
-        </Grid>
-      </Grid>
-    </Card>
+      </Card>
+      <br />
+      <DevicesList
+        devices={devices}
+        refreshPassword={refreshPassword}
+        updateSaverRuleStatus={updateSaverRuleStatus}
+        del={deleteDevice}
+      />
+    </>
   );
 }
-
-//     <!-- Table  -->
-//     <div class="row">
-//       <card>
-//         <div slot="header">
-//           <h4 class="card-title">Devices</h4>
-//         </div>
-
-//         <el-table :data="$store.state.devices">
-//           <el-table-column label="#" min-width="50" align="center">
-//             <div slot-scope="{ $index }">
-//               {{ $index + 1 }}
-//             </div>
-//           </el-table-column>
-
-//           <el-table-column prop="name" label="Name"></el-table-column>
-
-//           <el-table-column prop="dId" label="Device Id"></el-table-column>
-
-//           <el-table-column label="Password" style="margin-horizontal: 50px">
-//             <div slot-scope="{ row }">
-//               {{ row.password }}
-//               <el-tooltip
-//                 content="Update Password"
-//                 effect="light"
-//                 :open-delay="300"
-//                 placement="top"
-//               >
-//                 <base-button
-//                   type="success"
-//                   size="sm"
-//                   class="btn-link"
-//                   @click="refreshDevicePassword(row.dId)"
-//                 >
-//                   <i
-//                     style="margin-left: 5px; color: green"
-//                     class="tim-icons icon-refresh-01"
-//                   />
-//                 </base-button>
-//               </el-tooltip>
-//             </div>
-//           </el-table-column>
-
-//           <el-table-column
-//             prop="templateName"
-//             label="Template"
-//           ></el-table-column>
-
-//           <el-table-column label="Actions">
-//             <div slot-scope="{ row }">
-//               <el-tooltip
-//                 content="Saver Status Indicator"
-//                 style="margin-right: 10px"
-//               >
-//                 <i
-//                   class="fas fa-database"
-//                   :class="{
-//                     'text-success': row.saverRule.status,
-//                     'text-dark': !row.saverRule.status,
-//                   }"
-//                 ></i>
-//               </el-tooltip>
-
-//               <el-tooltip content="Database Saver">
-//                 <base-switch
-//                   @click="updateSaverRuleStatus(row.saverRule)"
-//                   :value="row.saverRule.status"
-//                   type="primary"
-//                   on-text="On"
-//                   off-text="Off"
-//                 />
-//               </el-tooltip>
-
-//               <el-tooltip
-//                 content="Delete"
-//                 effect="light"
-//                 :open-delay="300"
-//                 placement="top"
-//               >
-//                 <base-button
-//                   type="danger"
-//                   size="sm"
-//                   class="btn-link"
-//                   @click="deleteDevice(row)"
-//                 >
-//                   <i class="tim-icons icon-simple-remove" />
-//                 </base-button>
-//               </el-tooltip>
-//             </div>
-//           </el-table-column>
-//         </el-table>
-//       </card>
-//     </div>
-//   </div>
-// </template>
