0f1acf3ce5eb29523137764246158f80c6cff11f
Marcos Raimondi <marcosraimondi1@hotmail.com>
2023-02-01 17:12:36 -0300

    authentication middleware frontend level

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,19 +1,14 @@
 import { useEffect } from "react";
+import { store } from "../store/store";
+
 //If the user does not have a token, we send it to login
 //si el usuario no tiene token lo enviamos a login
 export default function authenticated(WrappedComponent) {
-  // store.dispatch("readToken");
-  // if (!store.state.auth) {
-  //   return redirect("/login");
-
-  // }
+  const token = store.getState().auth.token;
   return (props) => {
-    return <WrappedComponent {...props} />;
-    props.authenticated = true;
     useEffect(() => {
-      if (!props.authenticated) {
-        document.location.href = "/auth/login";
-      }
-    }, [props.authenticated]);
+      if (!token) document.location.href = "/auth/login";
+    }, [token]);
+    return <WrappedComponent {...props} />;
   };
 }
--- middleware/notAuthenticated.js
+++ middleware/notAuthenticated.js
@@ -1,18 +1,18 @@
 //If the user has a token, we send it to index
 //Si el usuario tiene token lo enviamos a index
 import { useEffect } from "react";
+import { store } from "../store/store";
 export default function notAuthenticated(WrappedComponent) {
   // store.dispatch('readToken');
   // if (store.state.auth) {
   //     return redirect('/dashboard')
   // }
+  const token = store.getState().auth.token;
   return (props) => {
-    return <WrappedComponent {...props} />;
-    props.authenticated = true;
     useEffect(() => {
-      if (props.authenticated) {
-        document.location.href = "/dashboard";
-      }
-    }, [props.authenticated]);
+      if (token) document.location.href = "/dashboard";
+    }, [token]);
+
+    return <WrappedComponent {...props} />;
   };
 }
